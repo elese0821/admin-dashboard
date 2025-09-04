3a59921d6b32f5fd3ab52c8205371381eb49f3c1
원영 <elese0821@naver.com>
2025-09-05 06:12:43 +0900

    세팅중

---
+++ src/layouts/Shell.tsx
@@ -0,0 +1,5 @@
+export default function Shell() {
+    return (
+        <div>Shell</div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/index.css";
+import App from "./App.tsx";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <StrictMode><App /></StrictMode>,
+    },
+]);
+
+createRoot(document.getElementById("root")!).render(
+    <RouterProvider router={router} />,
+);
